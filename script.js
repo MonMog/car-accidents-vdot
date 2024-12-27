ad32b125043a7a92b19ad98bf271f74e42964fe1
Mo <122757829+MonMog@users.noreply.github.com>
2024-12-27 14:13:33 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+const map = L.map('stateMap').setView([37.4316, -78.6569], 7);
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+const tiles = L.tileLayer(tileUrl, { attribution });     
 
+
+tiles.addTo(map);

b04822e81f4d556b7096e883088b0e9ad0232aee
Mo <122757829+MonMog@users.noreply.github.com>
2025-01-01 21:43:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,10 @@ os.makedirs(output_directory, exist_ok=True)
 
 
 options = webdriver.ChromeOptions()
-# options.add_argument("--headless")
+options.add_argument("--headless") 
+options.add_argument("--no-sandbox")  
+options.add_argument("--disable-dev-shm-usage")  
+options.add_argument("--disable-gpu")
 options.add_argument("--window-size=1320,780")
 options.add_experimental_option("prefs", {
     "download.default_directory": output_directory,
